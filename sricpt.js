43eeabe8b0a5d357c9b235b2e65d0a05f2e00022
Qazi-Ghulam-Mustafa <Qazi-Ghulam-Mustafa>
2026-03-19 02:09:12 +0500

    Commit

--- sricpt.js
+++ sricpt.js
@@ -1,3 +1,25 @@
+const navLink = document.querySelectorAll("header nav a")
+const logoLink = document.querySelector(".logo");
+const activePage = () => {
+    navLink.forEach(link => {
+        link.classList.remove('active')
+    })
+}
+navLink.forEach((link, idx) => {
+    link.addEventListener('click', () => {
+        if (!link.classList.contains('active')) {
+            activePage();
+
+            link.classList.add("active");
+        }
+    })
+})
+logoLink.addEventListener('click', () => {
+    if (!navLink[0].classList.contains('active'))
+        activePage();
+    navLink[0].classList.add("active");
+})
+
 const resumeBtns = document.querySelectorAll(".resume-btn");
 
 resumeBtns.forEach((btn, idx) => {
@@ -23,13 +45,20 @@ let index = 0;
 
 const activePortfolio = () => {
     const imgSlide = document.querySelector(".portfolio-carousel .img-slide");
+    const porfolioDetails = document.querySelectorAll(".portfolio-detail")
 
     imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
+    porfolioDetails.forEach(detail => {
+        detail.classList.remove("active")
+    });
+    porfolioDetails[index].classList.add("active")
 };
 arrowRight.addEventListener("click", () => {
     // console.log("chal rahe he")
     if (index < 4) {
         index++;
+        arrowLeft.classList.remove("disabled")
     }
     else {
         index = 5
@@ -41,11 +70,11 @@ arrowLeft.addEventListener("click", () => {
     // console.log("chal rahe he")
     if (index > 1) {
         index--;
-        arrowRight.classList.remove("disabled") 
+        arrowRight.classList.remove("disabled")
     }
     else {
         index = 0
-
+        arrowLeft.classList.add("disabled")
     }
     activePortfolio();
 })
